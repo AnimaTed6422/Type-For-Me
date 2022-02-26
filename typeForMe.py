d59a3ba6e90323071a364ac7e20a513d1c5c624d
anima.Ted() <89037615+AnimaTed6422@users.noreply.github.com>
2022-02-25 22:26:14 -0500

    Add Python Files

---
+++ typeForMe.py
@@ -0,0 +1,23 @@
+import keyboard
+import os
+import time
+os.system("title Type For Me!")
+def pause():
+    input("Press Enter To Continue...")
+while(True):
+    sen = input("Copy & Paste Text Below\n")
+    print("You have 5 Seconds")
+    time.sleep(1)
+    print(4)
+    time.sleep(1)
+    print(3)
+    time.sleep(1)
+    print(2)
+    time.sleep(1)
+    for char in sen:
+        if(char.isupper()):
+            keyboard.press_and_release("shift + " + char)
+        else:
+            keyboard.press_and_release(char)
+        time.sleep(0.0001)
+    pause()
